2610613fcdeedb03e507fdf941d8ecbd527c0003
Vasileios Rallis <vasilisrallis22@gmail.com>
2024-12-31 18:14:11 +0200

    initial commit

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,15 @@
+export default {
+    title: 'Vasileios Rallis',
+    description: 'Vasileios Rallis Portfolio',
+    themeConfig: {
+      nav: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about' },
+      ],
+      sidebar: [
+        { text: 'Home', link: '/' },
+        { text: 'About', link: '/about' },
+      ]
+    }
+  }
+  
---
+++ docs/index.md
@@ -0,0 +1,2 @@
+# Welcome to My Portfolio
+## Vasileios Rallis
